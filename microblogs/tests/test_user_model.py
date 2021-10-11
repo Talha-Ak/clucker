3c2e98a19985c79caa042e58694498832d1fa5e9
Talha Abdulkuddus <talha.abdulkuddus@kcl.ac.uk>
2021-10-11 22:24:46 +0100

    Refine Sign Up form
    - Add Sign Uo form unit tests.
    - Add password validation to sign up form.

--- microblogs/tests/test_user_model.py
+++ microblogs/tests/test_user_model.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils import timezone
-from .models import User, Post
+from microblogs.models import User, Post
 
 class PostModelTestCase(TestCase):
     def setUp(self):
