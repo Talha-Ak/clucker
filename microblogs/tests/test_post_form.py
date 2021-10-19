fd4da7ca116da0166c573530630514fb44c3a525
Talha Abdulkuddus <talha.abdulkuddus@kcl.ac.uk>
2021-10-19 14:47:19 +0100

    Refactor tests

--- microblogs/tests/test_post_form.py
+++ microblogs/tests/test_post_form.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django import forms
 from microblogs.forms import PostForm
-from microblogs.models import User
+from microblogs.models import User, Post
 
 class PostFormTestCase(TestCase):
     def setUp(self):
@@ -12,6 +12,12 @@ class PostFormTestCase(TestCase):
     def test_valid_post(self):
         form = PostForm(data=self.form_input)
         self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = PostForm()
+        self.assertIn('text', form.fields)
+        text_widget = form.fields['text'].widget
+        self.assertTrue(isinstance(text_widget, forms.Textarea))
 
     def test_post_form_model_validation(self):
         self.form_input['text'] = 'x' * 281
--- microblogs/tests/test_user_model.py
+++ microblogs/tests/test_user_model.py
@@ -1,43 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from django.utils import timezone
-from microblogs.models import User, Post
-
-class PostModelTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='@johndoe',
-            first_name='John',
-            last_name='Doe',
-            email='johndoe@example.org',
-            password='Password123',
-            bio='This is a test bio.'
-        )
-        self.post = Post.objects.create(
-            author=self.user,
-            text='This is just some example text to test things out.',
-        )
-
-    def test_valid_post(self):
-        self._assert_post_is_valid()
-
-    def test_post_text_280_chars_valid(self):
-        self.post.text = 'x' * 280
-        self._assert_post_is_valid()
-
-    def test_post_text_281_chars_invalid(self):
-        self.post.text = 'x' * 281
-        self._assert_post_is_invalid()
-
-    def _assert_post_is_valid(self):
-        try:
-            self.post.full_clean()
-        except (ValidationError):
-            self.fail('Test user is invalid.')
-
-    def _assert_post_is_invalid(self):
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
+from microblogs.models import User
 
 class UserModelTestCase(TestCase):
     def setUp(self):
