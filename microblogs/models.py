e996238929a3426236d9798092835b986033e94a
Talha Abdulkuddus <talha.abdulkuddus@kcl.ac.uk>
2021-10-26 00:46:30 +0100

    Integrate Gravatar
    - Add libgravatar to User model for getting profile images
    - Redesign show_user.html to utilise a user_profile partial
    - Redesign user_list.html to make use of profile pictures

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from libgravatar import Gravatar
 
 class User(AbstractUser):
     username = models.CharField(
@@ -16,6 +17,12 @@ class User(AbstractUser):
     last_name = models.CharField(blank=False, max_length=50)
     email = models.EmailField(unique=True, blank=False)
     bio = models.CharField(unique=False, blank=True, max_length=520)
+
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
 
 class Post(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False)
