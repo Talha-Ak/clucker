3ec7f46238b0a844e054244e5e069938f48f423b
Talha Abdulkuddus <talha.abdulkuddus@kcl.ac.uk>
2021-11-09 14:48:22 +0000

    Add update_profile view and template

---
+++ microblogs/tests/views/test_update_profile_view.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from django.urls import reverse
+from microblogs.forms import ProfileUpdateForm
+from microblogs.models import User
+from microblogs.tests.helpers import reverse_with_next
+
+class UpdateProfileViewTestCase(TestCase):
+    """Test suite for update_profile view"""
+
+    fixtures = ['microblogs/tests/fixtures/default_user.json']
+
+    def setUp(self):
+        self.url = reverse('update_profile')
+        self.user = User.objects.get(username='@johndoe')
+        self.form_input = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johndoe@otherdomain.com',
+            'bio': 'Hello, this is my new bio.',
+        }
+
+    def test_update_profile_url(self):
+        self.assertEqual(self.url, '/update_profile/')
+
+    def test_get_update_profile(self):
+        self.client.login(username=self.user.username, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'update_profile.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, ProfileUpdateForm))
+        self.assertFalse(form.is_bound)
+
+    def test_get_sign_up_redirects_when_not_logged_in(self):
+        redirect_url = reverse_with_next('log_in', self.url)
+        response = self.client.get(self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_update_fail(self):
+        self.client.login(username=self.user.username, password='Password123')
+        self.form_input['email'] = 'bad@$invalid@email'
+        response = self.client.post(self.url, self.form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'update_profile.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, ProfileUpdateForm))
+        self.assertTrue(form.is_bound)
+        self.assertNotEqual(self.user.email, self.form_input['email'])
+
+    def test_update_pass(self):
+        self.client.login(username=self.user.username, password='Password123')
+        response = self.client.post(self.url, self.form_input, follow=True)
+        response_url = reverse('feed')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'feed.html')
+        self.user = User.objects.get(username='@johndoe')  # Re-fetch user
+        self.assertEqual(self.user.email, self.form_input['email'])
+        self.assertEqual(self.user.bio, self.form_input['bio'])
--- microblogs/views.py
+++ microblogs/views.py
@@ -2,54 +2,18 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import SignUpForm, LogInForm, PostForm
+from .forms import SignUpForm, LogInForm, PostForm, ProfileUpdateForm
 from .models import User, Post
 from .helpers import login_prohibited
 
 @login_prohibited
 def home(request):
-    """View for GETting the homepage"""
+    """View for getting the homepage"""
     return render(request, 'home.html')
-
-@login_required
-def feed(request):
-    """View for GETting the user's main feed."""
-    form = PostForm()
-    current_user = request.user
-    posts = Post.objects.filter(author=current_user)
-    return render(request, 'feed.html', {'form': form, 'posts': posts})
-
-def new_post(request):
-    """View for POSTing a Post from a user."""
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        # Only save message if user logged in and within post text limit.
-        if not request.user.is_authenticated:
-            messages.add_message(request, messages.ERROR, "You need to be logged in to make a post.")
-        elif not form.is_valid():
-            messages.add_message(request, messages.ERROR, "Your post is too long. Maximum length is 280 characters.")
-        else:
-            form.save(request.user)
-        return redirect('feed')
-
-@login_required
-def user_list(request):
-    """View for GETting the user list page."""
-    return render(request, 'user_list.html', { 'users': User.objects.filter(is_superuser=False) })
-
-@login_required
-def show_user(request, user_id):
-    """View for GETting a specific user's page."""
-    try:
-        user = User.objects.get(id=user_id)
-        posts = Post.objects.filter(author=user)
-        return render(request, 'show_user.html', { 'user': user, 'posts': posts })
-    except User.DoesNotExist:
-        return redirect('user_list')
 
 @login_prohibited
 def log_in(request):
-    """View for GETting the login page, and for POSTing the completed login form"""
+    """View for getting the login page, and for posting the completed login form"""
     if request.method == 'POST':
         form = LogInForm(request.POST)
         next = request.POST.get('next') or '';
@@ -70,13 +34,13 @@ def log_in(request):
     return render(request, 'log_in.html', {'form': form, 'next': next})
 
 def log_out(request):
-    """View for GETting the log out page, which redirects to home."""
+    """View for getting the log out page, which redirects to home."""
     logout(request)
     return redirect(home)
 
 @login_prohibited
 def sign_up(request):
-    """View for GETting the sign up page, and for POSTing the completed form>"""
+    """View for getting the sign up page, and for posting the completed form>"""
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         # Check the validation of the form, and sign up user if OK.
@@ -88,3 +52,54 @@ def sign_up(request):
     else:
         form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
+
+@login_required
+def feed(request):
+    """View for getting the user's main feed."""
+    form = PostForm()
+    current_user = request.user
+    posts = Post.objects.filter(author=current_user)
+    return render(request, 'feed.html', {'form': form, 'posts': posts})
+
+def new_post(request):
+    """View for posting a Post from a user."""
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        # Only save message if user logged in and within post text limit.
+        if not request.user.is_authenticated:
+            messages.add_message(request, messages.ERROR, "You need to be logged in to make a post.")
+        elif not form.is_valid():
+            messages.add_message(request, messages.ERROR, "Your post is too long. Maximum length is 280 characters.")
+        else:
+            form.save(request.user)
+    return redirect('feed')
+
+@login_required
+def user_list(request):
+    """View for getting the user list page."""
+    return render(request, 'user_list.html', { 'users': User.objects.filter(is_superuser=False) })
+
+@login_required
+def show_user(request, user_id):
+    """View for getting a specific user's page."""
+    try:
+        user = User.objects.get(id=user_id)
+        posts = Post.objects.filter(author=user)
+        return render(request, 'show_user.html', { 'user': user, 'posts': posts })
+    except User.DoesNotExist:
+        return redirect('user_list')
+
+@login_required
+def update_profile(request):
+    """View for getting update profile page, and posting update form."""
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        # Check the validation of the form, and update user if OK.
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Your profile was updated.")
+            return redirect('feed')
+    # If a GET request, create new form.
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'update_profile.html', {'form': form})
